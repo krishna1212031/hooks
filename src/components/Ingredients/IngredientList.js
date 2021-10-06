1153c605c0aca933dcc0c4f5fa143b52cafc4dcb
krishna <krishna1212031@gmail.com>
2021-10-06 16:16:01 +0530

    third

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -8,7 +8,14 @@ const IngredientList = props => {
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-       {/* 03 */}
+      {props.ingredients.map(ig => (
+          <li key={ig.id} 
+          onClick={props.onRemoveItem.bind(this, ig.id)}
+          >
+            <span>{ig.title}</span>
+            <span>{ig.amount}x</span>
+          </li>
+        ))}
       </ul>
     </section>
   );

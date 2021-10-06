0f2c267e1954be28c9554626ce0e9dd25afb601c
krishna <krishna1212031@gmail.com>
2021-10-06 16:10:51 +0530

    second

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -14,7 +14,29 @@ const IngredientForm = React.memo((props) => {
   return (
     <section className="ingredient-form">
       <Card>
-       {/* 02 */}
+      <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
       </Card>
     </section>
   );
